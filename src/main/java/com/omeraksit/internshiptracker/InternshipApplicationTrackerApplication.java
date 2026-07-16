4b1e478690a875ba6c8ff9ae314bcac09734234f
ahmethan565 <omeraksit30@gmail.com>
2026-07-16 18:05:42 +0300

    Initialize Spring Boot project

---
+++ src/main/java/com/omeraksit/internshiptracker/InternshipApplicationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.omeraksit.internshiptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipApplicationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipApplicationTrackerApplication.class, args);
+	}
+
+}
